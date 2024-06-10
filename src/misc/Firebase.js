a27a79c18f12da689ad466bb8dacf88d3a52f650
Amaan's Git <shaikhhunt7262@gamil.com>
2024-06-10 15:04:33 +0530

    Initialized firebase project

---
+++ src/misc/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const config ={
+    apiKey: "AIzaSyCl1cyCfhPXOnVgwUp8S3lxip2akDxKR20",
+    authDomain: "my-chat-app-ced10.firebaseapp.com",
+    projectId: "my-chat-app-ced10",
+    storageBucket: "my-chat-app-ced10.appspot.com",
+    messagingSenderId: "1060634651118",
+    appId: "1:1060634651118:web:69118dcdc167241610c6ff",
+    measurementId: "G-5RW2PRWG8Y"
+  };
+
+  const app =firebase.initializeApp(config)
