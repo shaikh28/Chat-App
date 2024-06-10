11c920d7c22e6f1878e5fa475e4472b5b6222834
Amaan's Git <shaikhhunt7262@gamil.com>
2024-06-10 12:37:40 +0530

    Sacffolding the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss';
+import React from 'react'
+function App() {
+  return (
+   <div>
+
+   </div>
+  );
+}
+
+export default App;
